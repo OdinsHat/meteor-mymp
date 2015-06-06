4a48af50230064c076dc7c9d709d2fc6cf0e6ba7
Doug Bromley <doug.bromley@gmail.com>
2015-06-06 11:49:22 +0100

    Completely refactorin and switching round the publications back into a single one

--- server/publications.js
+++ server/publications.js
@@ -1,8 +1,9 @@
 Meteor.publish('twfySearch', function(postcode){
   var self = this;
+  var mp = {};
 
   try {
-    var resp = HTTP.get('http://www.theyworkforyou.com/api/getMP', {
+    var basicMpResp = HTTP.get('http://www.theyworkforyou.com/api/getMP', {
       params: {
         'postcode': postcode,
         'output': 'js',
@@ -10,37 +11,28 @@ Meteor.publish('twfySearch', function(postcode){
       }
     });
 
-    console.log(resp);
-    var mp = resp.data;
-    if (mp.office[0].position) {
+    mp.basic = basicMpResp.data;
+
+    if (mp.orespDetailffice) {
       mp.has_office = true;
       mp.position = mp.office[0].position;
     }
+
+    var detailMpResp = HTTP.get('http://www.theyworkforyou.com/api/getMPInfo', {
+      params: {
+      'id': mp.basic.person_id,
+      'output': 'js',
+      'key': TWFYAPIKEY
+      }
+    });
+
+    mp.detail = detailMpResp.data;
+
+
     console.log(mp);
     self.added('mp', Random.id(), mp);
     self.ready();
 
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-
-Meteor.publish('twfyDetail', function(personId){
-  var self = this;
-
-  try {
-    var respDetail = HTTP.get('http://www.theyworkforyou.com/api/getMPInfo', {
-      params: {
-        'id': personId,
-        'output': 'js',
-        'key': TWFYAPIKEY
-      }
-    });
-
-    console.log(respDetail);
-
-    var mp = resp.data;  
   } catch (error) {
     console.log(error);
   }
